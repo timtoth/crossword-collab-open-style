97f863ad9516059bc8126b12fcd3f13d17baa85f
Tim Toth <tim.toth13@gmail.com>
2022-03-29 10:52:41 -0600

    The whole thing
    Simply run the first line on the puzzle you wish and then run the rest

---
+++ main.js
@@ -0,0 +1,6 @@
+window.open($('iframe').attributes[3].nodeValue);
+$('.clues-scroll').css('height', '1300');
+$('.clue-list').css('height', '1300');
+$('.clue-list').css('height', '1300');
+$('.clue').css('font-size', '24px');
+$('.clue-bar').remove();
